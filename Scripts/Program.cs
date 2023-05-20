966ca348631bd2b8598d5fbea20e084cf2d3f7a7
FeM <philippe.makhroff@gmail.com>
2023-05-20 14:06:35 +0200

    project init

---
+++ Scripts/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PhilippIO.Scripts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
